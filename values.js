01517d9d68b86f95fe1bd7de2732cff57cab5905
Danilo Kulišić <danilokulisic4@gmail.com>
2025-09-14 00:33:44 +0200

    putting console.log in comment

--- values.js
+++ values.js
@@ -57,12 +57,12 @@ while (triangle2 <= "#######") {
 
 for (let number = 1; number <= 100; number = number + 1) {
     if (number % 3 === 0 && number % 5 === 0) {
-        console.log ("FizzBuzz");
+        //console.log ("FizzBuzz");
     } else if (number % 3 === 0) {
-        console.log("Fizz");
+        //console.log("Fizz");
     } else if (number % 5 === 0) {
-        console.log ("Buzz");
+       //console.log ("Buzz");
     } else {
-        console.log(number); //shows numbers that are not divisible neither with 3 or 5
+      //console.log(number); //shows numbers that are not divisible neither with 3 or 5
     }
 }
