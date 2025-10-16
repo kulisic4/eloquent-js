c8ec63edb8ba484b9e32ad9c3465c0c3c7a2f50c
Danilo Kulišić <danilokulisic4@gmail.com>
2025-10-16 13:19:13 +0200

    adding DataStructures and dataSets.js

---
+++ DataStructures/dataSets.js
@@ -0,0 +1,8 @@
+//storing numbers in the array listOfNumbers
+let listOfNumbers = [2, 3, 5, 7, 11];
+
+console.log(listOfNumbers[2]);
+
+//JS first calculates the expression in brackets...
+//2 - 1 = 1 ---> than JS looks for the element in index 1, which is 3
+console.log(listOfNumbers[2 - 1]);
